3058c78c8dd006ba79a1f76679bbf36b1a9d17d5
Radu Florescu <Radu.Florescu@isams.co.uk>
2018-04-25 15:30:40 +0100

    Added ability to test integration and some more complex functions.

---
+++ Source/CalcEngine.Tests/BasicExcelFunctionTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CalcEngine.Tests
+{
+    public class BasicExcelFunctionTests
+    {
+        [Fact]
+        public void supported_functions()
+        {
+            CalcEngine calcEngine = new CalcEngine();
+            calcEngine.Test("=SUM(1,2,3)", 6);
+            calcEngine.Test("=SUM(6)", 6);
+            calcEngine.Test("=ABS(-1)", 1);
+            calcEngine.Test("=ABS(1)", 1);
+            calcEngine.Test("=AVERAGE(1,2)", 1.5);
+            calcEngine.Test("CEILING(1.8)", Math.Ceiling(1.8));
+            calcEngine.Test("=COUNT(1,2,3)", 3);
+        }
+
+        [Fact(Skip = "Should not be run, this if only done for doc purpose")]
+        public void not_supported_functions()
+        {
+            CalcEngine calcEngine = new CalcEngine();
+            calcEngine.Test("=ARABIC(\"VII\")", 7);
+            calcEngine.Test("=BASE(10,2)", "1010");
+        }
+    }
+}
---
+++ Source/CalcEngine.Tests/ComplexExcelFunctionTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace CalcEngine.Tests
+{
+    public class ComplexExcelFunctionTests
+    {
+        [Fact]
+        public void supported_functions()
+        {
+            CalcEngine calcEngine = new CalcEngine();
+            calcEngine.Test("=SUM(1,2,AVERAGE(1,2) + 3)", 7.5);
+            
+        }
+    }
+}
